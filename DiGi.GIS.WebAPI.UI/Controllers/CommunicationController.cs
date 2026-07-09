f23276078722a37610a4df40d89d44a8f6ec8f4d
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2026-07-09 11:09:56 +0200

    Sync changes

--- DiGi.GIS.WebAPI.UI/Controllers/CommunicationController.cs
+++ DiGi.GIS.WebAPI.UI/Controllers/CommunicationController.cs
@@ -10,7 +10,6 @@ using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading;
